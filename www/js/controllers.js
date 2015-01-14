1c6c1e09895a794384a973821f93fcb42865cde2
sukhadia <neelsukhadia@gmail.com>
2015-01-14 10:07:12 -0600

    Removed JQuery use!!

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -50,6 +50,51 @@ angular.module('starter.controllers', ['ionic', 'services'])
       return window.cordova || window.Cordova;
   };
 
+  $scope.changePic = function(employee) {
+      if (!navigator.camera) {
+          alert("Camera API not supported", "Error");
+          return;
+      }
+      var options =   {   
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Album
+            encodingType: 0,     // 0=JPG 1=PNG
+            cameraDirection: 1  //NOTE: 1 is for Front-facing camera (0 - for back-facing, defalut?). 
+                        //Note this is advisory only, may not always work 
+                        //if the device does not support it.
+        };
+      navigator.camera.getPicture(function (imgData) {
+         $scope.$apply(function() {
+            employee.pic = "data:image/jpeg;base64,"+imgData;
+            console.log('****************EMPLOYEE PIC:\n\n\n' + employee.pic);
+         });
+      }, function (err) {
+         if (!(window.cordova || window.Cordova)) {
+          alert('Error taking picture', 'Error');
+         }
+      }, options);
+  };
+
+  $scope.addToContacts = function(employee) {
+    if (!navigator.contacts) {
+        alert("Contacts API not supported", "Error");
+        return;
+    }
+
+    var fullName = employee.firstName + ' ' + employee.lastName,
+        phoneNumbers = [];
+    phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+    phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true);
+    var contact = navigator.contacts.create({
+        displayName: fullName, 
+        nickname: fullName,
+        name: {givenName: employee.firstName, familyName: employee.lastName},
+        phoneNumbers: phoneNumbers
+      });
+    contact.save();
+    alert('Contact Saved!');
+  };
 
 }])
 
@@ -84,69 +129,5 @@ angular.module('starter.controllers', ['ionic', 'services'])
         console.log('Error: ' + err);
       });
   };
-}])
 
-//For some reason a regular click handler (with 'ng=click') wouldn't work for Contacts and Camera
-//So created directives for the same (based on some suggestions on StackOverflow).
-.directive('contacts', function() {
- return {
-    restrict: 'A',
-    require: 'ngModel',
-    link: function(scope, elm, attrs, ctrl) {
-      var employee = scope.employee;
-      elm.on('click', function() {
-        if (!navigator.contacts) {
-            alert("Contacts API not supported", "Error");
-            return;
-        }
-
-        var fullName = employee.firstName + ' ' + employee.lastName,
-            phoneNumbers = [];
-        phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
-        phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true);
-        var contact = navigator.contacts.create({
-            displayName: fullName, 
-            nickname: fullName,
-            name: {givenName: employee.firstName, familyName: employee.lastName},
-            phoneNumbers: phoneNumbers
-          });
-        contact.save();
-        alert('Contact Saved!');
-      });
-    }
-  };
-})
-
-.directive('camera', function() {
- return {
-    restrict: 'A',
-    require: 'ngModel',
-    link: function(scope, elm, attrs, ctrl) {
-      var employee = scope.employee;
-      elm.on('click', function() {
-        if (!navigator.camera) {
-            alert("Camera API not supported", "Error");
-            return;
-        }
-        var options =   {   
-              quality: 50,
-              destinationType: Camera.DestinationType.DATA_URL,
-              sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Album
-              encodingType: 0,     // 0=JPG 1=PNG
-              cameraDirection: 1  //NOTE: 1 is for Front-facing camera (0 - for back-facing, defalut?). 
-                          //Note this is advisory only, may not always work 
-                          //if the device does not support it.
-          };
-        navigator.camera.getPicture(function (imgData) {
-           scope.$apply(function() {
-              $('#emp-pic-'+employee.id).attr('src', "data:image/jpeg;base64,"+imgData);
-           });
-        }, function (err) {
-           if (!(window.cordova || window.Cordova)) {
-            alert('Error taking picture', 'Error');
-           }
-        }, options);
-     });
-    }
-  };
-});
+}]);
